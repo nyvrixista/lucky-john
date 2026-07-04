a1ba3e88440608278b76b0bd0a2bca1508e24a0b
nyvrixista <gavrilcatalin11@gmail.com>
2026-07-04 15:36:13 +0300

    Added a new backgroud for Level 1.

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
-    const canvas = document.querySelector('canvas')
-    const c = canvas.getContext('2d')
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-    canvas.width = 1024
-    canvas.height = 576
+canvas.width = 1024
+canvas.height = 576
 
-    const player = new Player()
-
-   const keys = {
-    w: {
-        pressed: false
-    },
-    a: {
-        pressed: false
-    }, 
-    d: {
-        pressed: false
-    },
-   }
-    function animate() {
-        window.requestAnimationFrame(animate)
-        c.fillStyle = 'white'
-        c.fillRect(0, 0, canvas.width, canvas.height)
-
-        player.velocity.x = 0
-        if (keys.d.pressed) player.velocity.x = 5
-        else if (keys.a.pressed) player.velocity.x = -5
-        
-        player.draw()
-        player.update()
-        
-       
+class Sprite {
+    constructor({position}) {
+        this.position = position
+        this.image = new Image()
+        this.image.src = 'img/backgroundLevel1.png'
     }
 
-    animate()
+    draw(){
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+
+const backgroundLevel1 = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    }
+})
+
+const player = new Player()
+
+const keys = {
+    w: { pressed: false },
+    a: { pressed: false },
+    d: { pressed: false }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    backgroundLevel1.draw()
+
+    player.velocity.x = 0
+    if (keys.d.pressed) player.velocity.x = 5
+    else if (keys.a.pressed) player.velocity.x = -5
+
+    player.update()
+    player.draw()
+}
+
+animate()
